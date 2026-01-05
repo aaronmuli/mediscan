3d4eff72aca5abdc47b8b134371ac96af2402ddd
aaronmuli <zionmurlley@gmail.com>
2026-01-05 15:43:15 +0200

    changed port to 7860

--- server.py
+++ server.py
@@ -86,4 +86,4 @@ if __name__ == '__main__':
     if os.getenv("FLASK_ENV") == "development":
         app.run(debug=True, port=5000)
     else:
-        app.run(port=5000)
+        app.run(host='0.0.0.0', port=7860)
